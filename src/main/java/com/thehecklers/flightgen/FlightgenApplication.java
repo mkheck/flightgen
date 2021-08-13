9216da1abfceaafc949433e8afabefc0f0c70e68
Mark Heckler <mark.heckler@gmail.com>
2021-08-13 16:02:46 -0500

    Added code for clean disconnection of RSocket clients

--- src/main/java/com/thehecklers/flightgen/FlightgenApplication.java
+++ src/main/java/com/thehecklers/flightgen/FlightgenApplication.java
@@ -2,11 +2,13 @@ package com.thehecklers.flightgen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class FlightgenApplication {
 
 	public static void main(String[] args) {
+		Hooks.onErrorDropped(err -> System.out.println("RSocket client disconnected."));
 		SpringApplication.run(FlightgenApplication.class, args);
 	}
 
